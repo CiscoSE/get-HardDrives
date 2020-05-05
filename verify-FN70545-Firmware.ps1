2c280cff584218ed5e8a3b0c2b2614ea2798e7a1
srehling <srehling@cisco.com>
2020-05-05 09:08:30 -0400

    Added Licensing to file, and updated comment based help

--- verify-FN70545-Firmware.ps1
+++ verify-FN70545-Firmware.ps1
@@ -1,4 +1,35 @@
-﻿[cmdletbinding()]
+﻿<#
+.NOTES
+Copyright (c) 2019 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+.PARAMETER CimcIPs
+This can be an IP or resolvable name. You can provide more then one server using
+this option by including multiple names or IPs separated by commas. Do not 
+include any spaces between systems.
+
+.PARAMETER Cred
+If you already have a PSCredential object with the user name and password, 
+you can pass that using this variable. This is mostly used for developement
+so I don't have to keep typing the user name and password every time I run
+the script.
+
+All servers to be assessed by this script must use the same user name and password
+for access to the CIMC in order for this script to work.
+
+#>
+
+
+[cmdletbinding()]
 param(
     [parameter(mandatory=$true)][array]$CimcIPs,
     [parameter(mandatory=$true)][pscredential]$Cred
